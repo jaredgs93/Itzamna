01aab5034b2ae534638f8228fee2ccf7d26c8745
Jared Guerrero <jaredguerrero@MacBook-Air-de-Jared-2.local>
2024-12-07 15:50:07 -0500

    init-mongo modified for proper import of documents

--- src/data/init-mongo.sh
+++ src/data/init-mongo.sh
@@ -6,10 +6,10 @@ sleep 5
 
 # Importa datos a la colección `rules_evaluation`
 mongoimport --host localhost --db skills_evaluation --collection rules_evaluation \
-    --file /docker-entrypoint-initdb.d/rules_evaluation.json --jsonArray
+    --file /docker-entrypoint-initdb.d/rules_evaluation.json --jsonArray --drop
 
 # Importa datos a la colección `fuzzy_variables`
 mongoimport --host localhost --db skills_evaluation --collection fuzzy_variables \
-    --file /docker-entrypoint-initdb.d/fuzzy_variables.json --jsonArray
+    --file /docker-entrypoint-initdb.d/fuzzy_variables.json --jsonArray --drop
 
 echo "Datos importados correctamente."
