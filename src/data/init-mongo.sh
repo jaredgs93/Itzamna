70567e16bceb1d7dad99fe0dac9ac1ed231eb0da
Jared Guerrero <jaredguerrero@MacBook-Air-de-Jared-2.local>
2024-12-14 16:02:56 -0500

    Adjusted comments in init-mongo.sh

--- src/data/init-mongo.sh
+++ src/data/init-mongo.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Espera a que MongoDB esté disponible
-echo "Esperando a que MongoDB esté disponible..."
+# Wait for MongoDB to become available
+echo "Wait for MongoDB to become available..."
 sleep 5
 
-# Importa datos a la colección `rules_evaluation`
+# Import data into the collection `rules_evaluation`
 mongoimport --host localhost --db skills_evaluation --collection rules_evaluation \
     --file /docker-entrypoint-initdb.d/rules_evaluation.json --jsonArray --drop
 
-# Importa datos a la colección `fuzzy_variables`
+# Import data into the collection `fuzzy_variables`
 mongoimport --host localhost --db skills_evaluation --collection fuzzy_variables \
     --file /docker-entrypoint-initdb.d/fuzzy_variables.json --jsonArray --drop
 
-echo "Datos importados correctamente."
+echo "Data imported correctly."
